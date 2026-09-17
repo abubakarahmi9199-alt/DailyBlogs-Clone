f944e1be38874de7ba49dbb2e1bcb896eafec833
abubakar9199 <abubakaraabhi9199@gmail.com>
2026-09-18 01:58:54 +0500

    Create header section

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("DailyBlogs Clone");
+```javascript
+// Page load hone par message
+console.log("Daily Blogs website loaded successfully!");
+
+
+// Start Reading button
+const startButton = document.querySelector(".btn");
+
+startButton.addEventListener("click", function () {
+
+    console.log("User clicked Start Reading");
+
+});
+```
